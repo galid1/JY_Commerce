2cc7ba08cbfe50bc1c7361cb4b41824186fd420c
galid1 <galid9619@gmail.com>
2020-05-03 19:53:57 +0900

    add order entity

--- src/main/java/com/galid/commerce/domains/order/domain/OrderEntity.java
+++ src/main/java/com/galid/commerce/domains/order/domain/OrderEntity.java
@@ -1,4 +1,28 @@
 package com.galid.commerce.domains.order.domain;
 
-public class OrderEntity {
+import com.galid.commerce.common.config.logging.BaseEntity;
+import com.galid.commerce.domains.delivery.domain.DeliveryEntity;
+import com.galid.commerce.domains.member.domain.MemberEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "order")
+@Getter
+public class OrderEntity extends BaseEntity {
+    @Id @GeneratedValue
+    private Long id;
+    private int totalAmount;
+    private OrderStatus status;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberEntity orderer;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private DeliveryEntity deliveryInformation;
+    @OneToMany
+    private List<OrderItemEntity> orderItemList;
 }
