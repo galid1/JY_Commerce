e17a22d40648778b3d85903745e0ea80d47cee32
galid1 <galid9619@gmail.com>
2020-05-03 19:53:42 +0900

    add annotation to MemberEntity

--- src/main/java/com/galid/commerce/domains/member/domain/MemberEntity.java
+++ src/main/java/com/galid/commerce/domains/member/domain/MemberEntity.java
@@ -1,16 +1,18 @@
 package com.galid.commerce.domains.member.domain;
 
+import com.galid.commerce.common.config.logging.BaseEntity;
 import com.galid.commerce.common.value.Address;
 import lombok.Getter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "member")
 @Getter
-public class MemberEntity {
+public class MemberEntity extends BaseEntity {
+    @Id @GeneratedValue
     private Long id;
     private String name;
+    @Embedded
     private Address address;
 }
