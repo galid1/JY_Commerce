6c729600159ba49575ea9004cf44498d662ec32f
galid1 <galid9619@gmail.com>
2020-05-03 19:38:42 +0900

    add item entity

--- src/main/java/com/galid/commerce/domains/item/domain/ItemEntity.java
+++ src/main/java/com/galid/commerce/domains/item/domain/ItemEntity.java
@@ -1,5 +1,22 @@
 package com.galid.commerce.domains.item.domain;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "item")
+@Getter
 public class ItemEntity {
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<CategoryEntity> categoryList;
 
 }
