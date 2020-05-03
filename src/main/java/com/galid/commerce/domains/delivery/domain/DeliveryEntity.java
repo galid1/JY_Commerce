58569ac5760c575ee8038b6366a9a63bf9ef55b8
galid1 <galid9619@gmail.com>
2020-05-03 19:52:05 +0900

    add DeliveryEntity

--- src/main/java/com/galid/commerce/domains/delivery/domain/DeliveryEntity.java
+++ src/main/java/com/galid/commerce/domains/delivery/domain/DeliveryEntity.java
@@ -1,4 +1,16 @@
 package com.galid.commerce.domains.delivery.domain;
 
-public class DeliveryEntity {
+import com.galid.commerce.common.config.logging.BaseEntity;
+import com.galid.commerce.common.value.Address;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "delivery")
+@Getter
+public class DeliveryEntity extends BaseEntity {
+    private Long id;
+    private Address address;
 }
