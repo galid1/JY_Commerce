9c9a4e9aff723783cf0e10bde408e6eb5edb59e1
galid1 <galid9619@gmail.com>
2020-05-03 19:56:57 +0900

    add BaseEntity

---
+++ src/main/java/com/galid/commerce/common/config/logging/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.galid.commerce.common.config.logging;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
