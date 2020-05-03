586724352f6ffaf1fad2deeea6a7ca5f8e0130e7
galid1 <galid9619@gmail.com>
2020-05-03 19:54:06 +0900

    add orderItem entity

--- src/main/java/com/galid/commerce/domains/order/domain/OrderItemEntity.java
+++ src/main/java/com/galid/commerce/domains/order/domain/OrderItemEntity.java
@@ -1,4 +1,21 @@
 package com.galid.commerce.domains.order.domain;
 
-public class OrderItemEntity {
+import com.galid.commerce.common.config.logging.BaseEntity;
+import com.galid.commerce.domains.item.domain.ItemEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+@Getter
+public class OrderItemEntity extends BaseEntity {
+    @Id @GeneratedValue
+    private Long id;
+    private int orderQuantity;
+    private int orderItemAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private ItemEntity item;
 }
