f7951022e6924cb934ef49de627d1066c2e0b530
galid1 <galid9619@gmail.com>
2020-05-03 19:27:53 +0900

    add Category entity

--- src/main/java/com/galid/commerce/domains/item/domain/CategoryEntity.java
+++ src/main/java/com/galid/commerce/domains/item/domain/CategoryEntity.java
@@ -1,4 +1,17 @@
 package com.galid.commerce.domains.item.domain;
 
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+@Getter
 public class CategoryEntity {
+    @Id @GeneratedValue
+    private Long id;
+    private String categoryName;
 }
