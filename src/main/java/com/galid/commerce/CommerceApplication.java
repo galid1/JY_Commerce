05be26bc4e4700abd96d3db9cf65899f8d63f6a6
galid1 <galid9619@gmail.com>
2020-05-03 19:54:59 +0900

    enable JpaAuditing

--- src/main/java/com/galid/commerce/CommerceApplication.java
+++ src/main/java/com/galid/commerce/CommerceApplication.java
@@ -2,8 +2,10 @@ package com.galid.commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CommerceApplication {
 
     public static void main(String[] args) {
