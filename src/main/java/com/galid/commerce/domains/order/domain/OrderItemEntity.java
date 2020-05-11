1fa8686b9392082b27c6ee13e142c4de943d46fc
jjy <galid1@naver.com>
2020-05-11 22:50:14 +0900

    modify OrderItemEntity by JJY - 생성자, 비즈니스로직 추가

--- src/main/java/com/galid/commerce/domains/order/domain/OrderItemEntity.java
+++ src/main/java/com/galid/commerce/domains/order/domain/OrderItemEntity.java
@@ -3,6 +3,7 @@ package com.galid.commerce.domains.order.domain;
 import com.galid.commerce.common.config.logging.BaseEntity;
 import com.galid.commerce.domains.item.domain.ItemEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,4 +22,20 @@ public class OrderItemEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private ItemEntity item;
+
+    @Builder
+    public OrderItemEntity(ItemEntity item, int orderQuantity) {
+        this.setItem(item, orderQuantity);
+        this.orderQuantity = orderQuantity;
+        this.setOrderItemAmount();
+    }
+
+    private void setItem(ItemEntity item, int orderQuantity) {
+        item.removeStockQuantity(orderQuantity);
+        this.item = item;
+    }
+
+    private void setOrderItemAmount() {
+        this.orderItemAmount = this.item.getPrice() * orderQuantity;
+    }
 }
