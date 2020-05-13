edb16854f71e7b9a4240e55549bfb9818b6e8759
jjy <galid1@naver.com>
2020-05-13 23:09:17 +0900

    modify CategoryEntity - 기본 생성자 어노테이션 추가

--- src/main/java/com/galid/commerce/domains/item/domain/CategoryEntity.java
+++ src/main/java/com/galid/commerce/domains/item/domain/CategoryEntity.java
@@ -1,7 +1,10 @@
 package com.galid.commerce.domains.item.domain;
 
 import com.galid.commerce.common.config.logging.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,8 +14,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "categories")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CategoryEntity extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
     private String categoryName;
+
 }
