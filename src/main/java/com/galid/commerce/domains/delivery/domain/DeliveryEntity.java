c8201a7ee69c43ba94e529e96370209911e95c02
jjy <galid1@naver.com>
2020-05-13 23:06:44 +0900

    modify DeliveryEntity - 생성자 추가

--- src/main/java/com/galid/commerce/domains/delivery/domain/DeliveryEntity.java
+++ src/main/java/com/galid/commerce/domains/delivery/domain/DeliveryEntity.java
@@ -3,6 +3,7 @@ package com.galid.commerce.domains.delivery.domain;
 import com.galid.commerce.common.config.logging.BaseEntity;
 import com.galid.commerce.common.value.Address;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +20,10 @@ public class DeliveryEntity extends BaseEntity {
 
     @Enumerated(value = EnumType.STRING)
     private DeliveryStatus status;
+
+    @Builder
+    public DeliveryEntity(Address address) {
+        this.address = address;
+        this.status = DeliveryStatus.READY_STATUS;
+    }
 }
