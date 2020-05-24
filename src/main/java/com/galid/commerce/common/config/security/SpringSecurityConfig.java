f954e9d2369218f5f07bfde882e5b9e7f12eee3d
jjy <galid1@naver.com>
2020-05-24 20:26:26 +0900

    add h2 console config

--- src/main/java/com/galid/commerce/common/config/security/SpringSecurityConfig.java
+++ src/main/java/com/galid/commerce/common/config/security/SpringSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -13,9 +15,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf()
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("!/h2-console/**"))
+                .and()
+                .headers()
+                .addHeaderWriter(new StaticHeadersWriter("X-Content-Security-Policy","script-src 'self'"))
+                .frameOptions().disable()
                 .disable()
             .authorizeRequests()
-                .antMatchers("/signIn", "/signUp")
+                .antMatchers("/h2-console/**", "/signIn", "/signUp")
                     .permitAll()
                 .anyRequest()
                     .authenticated()
