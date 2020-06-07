3df1c835da10ba4a10d287699172fb2ecb34bcf8
jjy <galid1@naver.com>
2020-06-07 22:41:49 +0900

    modify MemberController - 회원가입 핸들러 추가

--- src/main/java/com/galid/commerce/domains/member/presentation/MemberController.java
+++ src/main/java/com/galid/commerce/domains/member/presentation/MemberController.java
@@ -1,16 +1,31 @@
 package com.galid.commerce.domains.member.presentation;
 
+import com.galid.commerce.domains.member.service.MemberService;
+import com.galid.commerce.domains.member.service.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping(value = "/members")
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/signUp")
     public String getSignUpPage() {
         return "members/signUp";
+    }
+
+    @PostMapping("/members/signUp")
+    public String signUp(@ModelAttribute SignUpRequest request) {
+        memberService.signUp(request);
+        return "redirect:/signIn";
+    }
+
+    @GetMapping("/signIn")
+    public String getSignInPage() {
+        return "members/signIn";
     }
 }
