d763b4029a4bf5571e30cf2be014c32e01f3eda8
jjy <galid1@naver.com>
2020-06-07 22:40:28 +0900

    modify MemberRepository - 중복회원 검사를 위해, authId 쿼리메소드 추가

--- src/main/java/com/galid/commerce/domains/member/domain/MemberRepository.java
+++ src/main/java/com/galid/commerce/domains/member/domain/MemberRepository.java
@@ -2,5 +2,8 @@ package com.galid.commerce.domains.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByAuthId(String authId);
 }
