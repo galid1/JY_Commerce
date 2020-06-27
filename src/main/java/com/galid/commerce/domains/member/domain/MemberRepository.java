6d5ef7b6db4798c1eddf9d6a195f6a607e8d7299
jjy <galid1@naver.com>
2020-06-27 10:38:34 +0900

    modify MemberRepository - 회원 조회 쿼리 (한 로우를 조회하도록 변경)

--- src/main/java/com/galid/commerce/domains/member/domain/MemberRepository.java
+++ src/main/java/com/galid/commerce/domains/member/domain/MemberRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    Optional<MemberEntity> findByAuthId(String authId);
+    Optional<MemberEntity> findFirstByAuthId(String authId);
 }
