0cb6d0086fd69d1d675f0622d86eb3a2429eef71
jjy <galid1@naver.com>
2020-06-27 10:40:32 +0900

    add DatabaseUserDetailsService - SpringSecurity 데이터베이스를 통한 로그인 기능 추가

---
+++ src/main/java/com/galid/commerce/common/config/security/DatabaseUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.galid.commerce.common.config.security;
+
+import com.galid.commerce.domains.member.domain.MemberEntity;
+import com.galid.commerce.domains.member.domain.MemberRepository;
+import com.sun.tools.javac.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class DatabaseUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        MemberEntity findUser = memberRepository.findFirstByAuthId(username)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 ID 입니다."));
+
+        return new User(findUser.getAuthId(),
+                        findUser.getAuthPw(),
+                        List.of(new SimpleGrantedAuthority(findUser.getRole().getRole())));
+    }
+}
