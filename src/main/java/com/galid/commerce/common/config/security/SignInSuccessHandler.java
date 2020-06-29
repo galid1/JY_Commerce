a5e42766176d0294342cdfe25cb2fa57d6ec12d6
jjy <galid1@naver.com>
2020-06-29 22:20:48 +0900

    add SignInSuccessHandler

---
+++ src/main/java/com/galid/commerce/common/config/security/SignInSuccessHandler.java
@@ -0,0 +1,21 @@
+package com.galid.commerce.common.config.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignInSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        request
+            .getSession()
+            .setAttribute("login", true);
+
+        response.sendRedirect("/main");
+    }
+}
