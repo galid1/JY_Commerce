c6e79744b2d984cfa1f2dd1ff5068f78cffb0fb0
jjy <galid1@naver.com>
2020-06-29 21:06:53 +0900

    add JpaAuditingConfig - Controller Test시 jpaAuditing을 컨테이너에 포함하지 않기 위한 설정

---
+++ src/main/java/com/galid/commerce/common/config/auditing/JpaAuditConfig.java
@@ -0,0 +1,10 @@
+package com.galid.commerce.common.config.auditing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfig {
+}
