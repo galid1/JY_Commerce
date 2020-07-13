a42c9b0ecb1241f2bc37f2cae04234e505e6c14b
jjy <galid1@naver.com>
2020-07-13 22:56:23 +0900

    add Repository

---
+++ src/main/java/com/galid/commerce/domains/delivery/domain/DeliveryRepository.java
@@ -0,0 +1,6 @@
+package com.galid.commerce.domains.delivery.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<DeliveryEntity, Long> {
+}
---
+++ src/main/java/com/galid/commerce/domains/item/domain/ItemRepository.java
@@ -0,0 +1,6 @@
+package com.galid.commerce.domains.item.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
+}
