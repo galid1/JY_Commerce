00e869070b7b023b4d46b28a4536e8b38392dfbe
전준엽 <galid1@naver.com>
2020-08-02 10:22:56 +0900

    add
    - Item 생성 페이지 반환 및 생성 요청 처리 컨트롤러 생성

---
+++ src/main/java/com/galid/commerce/domains/item/presentation/ItemController.java
@@ -0,0 +1,28 @@
+package com.galid.commerce.domains.item.presentation;
+
+import com.galid.commerce.domains.item.domain.Book;
+import com.galid.commerce.domains.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String getNewItemPage(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String createItem(@ModelAttribute BookForm bookForm) {
+        itemService.saveItem(bookForm.toBookEntity());
+        return "redirect:/items/new";
+    }
+}
