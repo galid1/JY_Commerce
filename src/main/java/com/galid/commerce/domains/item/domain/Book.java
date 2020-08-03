b091206c5dcb1320d6fcae5a4b8eba31f66418e9
전준엽 <galid1@naver.com>
2020-08-03 12:03:06 +0900

    mod BookEntity에 Getter Setter, DiscriminatorValue 추가

--- src/main/java/com/galid/commerce/domains/item/domain/Book.java
+++ src/main/java/com/galid/commerce/domains/item/domain/Book.java
@@ -1,8 +1,17 @@
 package com.galid.commerce.domains.item.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@NoArgsConstructor
+@DiscriminatorValue("BOOK")
+@Getter
+@Setter
 public class Book extends ItemEntity{
     private String author;
     private String isbn;
