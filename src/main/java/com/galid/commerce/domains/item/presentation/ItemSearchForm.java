7cf38a820428cf226a9797458953e4c25b85b8eb
전준엽 <galid1@naver.com>
2020-08-15 11:19:32 +0900

    add 아이템 조회 폼

---
+++ src/main/java/com/galid/commerce/domains/item/presentation/ItemSearchForm.java
@@ -0,0 +1,11 @@
+package com.galid.commerce.domains.item.presentation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemSearchForm {
+    private String name;
+    private Sorter sorter;
+}
