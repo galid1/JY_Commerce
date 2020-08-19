3fcff48eb10b0928714902f51fe4954f7a18c65c
전준엽 <galid1@naver.com>
2020-08-19 16:44:45 +0900

    add Item Search Query

---
+++ src/main/java/com/galid/commerce/domains/item/domain/ItemQuery.java
@@ -0,0 +1,57 @@
+package com.galid.commerce.domains.item.domain;
+
+import com.galid.commerce.domains.item.service.ItemSearchForm;
+import com.galid.commerce.domains.item.presentation.Sorter;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.galid.commerce.domains.item.domain.QItemEntity.itemEntity;
+
+
+@Repository
+public class ItemQuery {
+    private JPAQueryFactory query;
+
+    public ItemQuery(EntityManager entityManager) {
+        this.query = new JPAQueryFactory(entityManager);
+    }
+
+    public List<ItemEntity> searchItem(ItemSearchForm searchForm)  {
+        return query
+                .selectFrom(itemEntity)
+                .where(nameLike(searchForm.getName()))
+                .orderBy(sorter(searchForm.getSorter()))
+                .fetch();
+    }
+
+    private Predicate nameLike(String name) {
+        if (name != null && name.length() > 0)
+            return itemEntity.name.like("%" + name + "%");
+        return null;
+    }
+
+    private OrderSpecifier sorter(Sorter sorter) {
+        if (sorter == null)
+            return itemEntity.createdDate.desc();
+
+        switch (sorter) {
+            case PRICE:
+                return itemEntity.price.desc();
+            case LATEST:
+                return itemEntity.createdDate.desc();
+            default:
+                return itemEntity.createdDate.desc();
+        }
+    }
+
+    public List<ItemEntity> findItemsByIds(List<Long> ids) {
+        return query.selectFrom(itemEntity)
+                .where(itemEntity.itemId.in(ids))
+                .fetch();
+    }
+}
