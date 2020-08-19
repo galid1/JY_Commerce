997aec71cd540b3971484ab0673cafef926dc626
전준엽 <galid1@naver.com>
2020-08-19 16:47:06 +0900

    mod
    - ItemDetails Page get 핸들러 추가
    - ItemList Page get 핸들러 추가

--- src/main/java/com/galid/commerce/domains/item/presentation/ItemController.java
+++ src/main/java/com/galid/commerce/domains/item/presentation/ItemController.java
@@ -1,30 +1,60 @@
 package com.galid.commerce.domains.item.presentation;
 
-import com.galid.commerce.domains.item.domain.Book;
+import com.galid.commerce.domains.item.domain.ItemEntity;
+import com.galid.commerce.domains.item.domain.ItemQuery;
+import com.galid.commerce.domains.item.service.AddBookForm;
+import com.galid.commerce.domains.item.service.ItemSearchForm;
 import com.galid.commerce.domains.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+    private final ItemQuery itemQuery;
 
     @GetMapping("/items/new")
     public String getNewItemPage(Model model) {
-        model.addAttribute("form", new BookForm());
+        model.addAttribute("form", new AddBookForm());
         return "items/createItemForm";
     }
 
     @PostMapping("/items/new")
-    public String createItem(@ModelAttribute @Valid BookForm bookForm) {
-        Long newItemId = itemService.saveItem(bookForm.toEntity());
-        return "redirect:/items/details?item_id=" + newItemId;
+    public String createItem(@ModelAttribute @Valid AddBookForm addBookForm) {
+        Long newItemId = itemService.saveItem(addBookForm.toEntity());
+        return "redirect:/items/"+ newItemId;
+    }
+
+    @GetMapping("/items")
+    public String getItemListPage(@ModelAttribute ItemSearchForm searchForm,
+                                  Model model) {
+        // 아이템 검색 form
+        if (searchForm == null)
+            model.addAttribute("itemSearchForm", new ItemSearchForm());
+        else
+            model.addAttribute("itemSearchForm", searchForm);
+
+        // 아이템 리스트
+        List<ItemEntity> items = itemQuery.searchItem(searchForm);
+        model.addAttribute("items", items);
+
+        return "items/itemList";
+    }
+
+    @GetMapping("/items/{itemId}")
+    public String getItemDetailsPage(@PathVariable("itemId") Long itemId,
+                                     Model model) {
+        ItemEntity item = itemService.findItem(itemId);
+        model.addAttribute("item", item);
+        return "items/itemDetails";
     }
 }
