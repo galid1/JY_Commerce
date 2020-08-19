0a65bd19de868a16be4b9ade448ad6aa94d83dd4
전준엽 <galid1@naver.com>
2020-08-19 16:40:03 +0900

    add 장바구니 수량 변경 폼

---
+++ src/main/java/com/galid/commerce/domains/cart/service/ModifyCartLineRequestForm.java
@@ -0,0 +1,11 @@
+package com.galid.commerce.domains.cart.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ModifyCartLineRequestForm {
+    private Long itemId;
+    private int orderCount;
+}
