b482a9d4e45b837013bb2cf04875b3696770e45d
전준엽 <galid1@naver.com>
2020-08-29 15:07:34 +0900

    add 장바구니 밸류 객체 추가

---
+++ src/main/java/com/galid/commerce/domains/cart/domain/CartLine.java
@@ -0,0 +1,22 @@
+package com.galid.commerce.domains.cart.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@NoArgsConstructor
+@Getter
+public class CartLine {
+    private Long cartId;
+    private Long itemId;
+    private Integer orderCount;
+
+    public CartLine(Long cartId, Long itemId, Integer orderCount) {
+        this.cartId = cartId;
+        this.itemId = itemId;
+        this.orderCount = orderCount;
+    }
+}
