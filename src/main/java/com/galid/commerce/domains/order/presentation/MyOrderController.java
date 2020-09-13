213b4b4e2b663a5a825c6d5f33b8f47f415d11ec
전준엽 <galid1@naver.com>
2020-09-13 09:47:39 +0900

    add 사용자 주문 컨트롤러

---
+++ src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
@@ -0,0 +1,27 @@
+package com.galid.commerce.domains.order.presentation;
+
+import com.galid.commerce.domains.member.service.MemberService;
+import com.galid.commerce.domains.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class MyOrderController {
+    private final MemberService memberService;
+    private final OrderService orderService;
+
+    @GetMapping("/my/orders")
+    public String getMyOrderListPage() {
+        return "myOrderList";
+    }
+
+    @GetMapping("/my/orders/{orderId}")
+    @ResponseBody
+    public String getMyOrderDetails(@PathVariable("orderId") Long orderId) {
+        return "OK";
+    }
+}
