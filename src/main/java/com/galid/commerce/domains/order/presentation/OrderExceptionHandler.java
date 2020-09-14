afb0f9f3f3d74407b35fd8e4672ae9134c7c55c7
전준엽 <galid1@naver.com>
2020-09-14 12:50:10 +0900

    add 주문 에러처리 핸들러 추가

---
+++ src/main/java/com/galid/commerce/domains/order/presentation/OrderExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.galid.commerce.domains.order.presentation;
+
+import com.galid.commerce.domains.item.domain.NotEnoughStockQuantityException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class OrderExceptionHandler {
+    @ExceptionHandler(NotEnoughStockQuantityException.class)
+    public String notEnoughStockQuantityExceptionHandler(NotEnoughStockQuantityException e,
+                                                         Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error/error";
+    }
+}
