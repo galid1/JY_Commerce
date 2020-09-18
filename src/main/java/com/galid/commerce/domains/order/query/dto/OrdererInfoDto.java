5775c5042976e27dea9c75ce8b70176c38770e69
전준엽 <galid1@naver.com>
2020-09-18 13:46:41 +0900

    add 주문자를 의미하는 dto 추가

---
+++ src/main/java/com/galid/commerce/domains/order/query/dto/OrdererInfoDto.java
@@ -0,0 +1,16 @@
+package com.galid.commerce.domains.order.query.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrdererInfoDto {
+    private Long ordererId;
+    private String name;
+    private String phone;
+}
