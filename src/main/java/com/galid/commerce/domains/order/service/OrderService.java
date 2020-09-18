c2109c6283247b7a2af72ed609c3c99f9b9cfec2
전준엽 <galid1@naver.com>
2020-09-18 13:51:31 +0900

    mod 주문 서비스에서 여러 상품 주문이 가능하도록 변경 및 리팩토링
    - 여러상품을 주문 가능하도록 변경
    - 주문 완료된 상품에 대해 장바구니 비우기 로직 추가
    - 주문자id 변수이름 ordererId로 변경

--- src/main/java/com/galid/commerce/domains/order/service/OrderService.java
+++ src/main/java/com/galid/commerce/domains/order/service/OrderService.java
@@ -1,50 +1,73 @@
 package com.galid.commerce.domains.order.service;
 
+import com.galid.commerce.domains.cart.service.CartService;
 import com.galid.commerce.domains.delivery.domain.DeliveryEntity;
 import com.galid.commerce.domains.item.domain.ItemEntity;
-import com.galid.commerce.domains.item.service.ItemService;
+import com.galid.commerce.domains.item.domain.ItemQuery;
+import com.galid.commerce.domains.item.domain.ItemRepository;
 import com.galid.commerce.domains.member.domain.MemberEntity;
-import com.galid.commerce.domains.member.domain.MemberRepository;
 import com.galid.commerce.domains.member.service.MemberService;
 import com.galid.commerce.domains.order.domain.OrderEntity;
 import com.galid.commerce.domains.order.domain.OrderItemEntity;
 import com.galid.commerce.domains.order.domain.OrderRepository;
+import com.galid.commerce.domains.order.query.dao.OrderDao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
     private final MemberService memberService;
-    private final ItemService itemService;
+    private final CartService cartService;
+    private final ItemRepository itemRepository;
 
-    @Transactional
-    public Long order(Long orderer, Long itemId, int count) {
-        MemberEntity memberEntity = memberService.findMember(orderer);
-        ItemEntity itemEntity = itemService.findItem(itemId);
+    public Long order(Long ordererId, OrderRequest orderRequest) {
+        // 엔티티 조회
+        MemberEntity orderer = memberService.findMember(ordererId);
 
+        // 배송지 생성
         DeliveryEntity deliveryEntity = DeliveryEntity.builder()
-                .address(memberEntity.getAddress())
+                .address(orderer.getAddress())
                 .build();
 
-        OrderItemEntity orderItemEntity = OrderItemEntity.builder()
-                .item(itemEntity)
-                .orderQuantity(count)
-                .build();
+        // 주문상품 생성
+        List<OrderItemEntity> orderItemEntityList = orderRequest.getOrderLineList()
+                .stream()
+                .map(ol -> {
+                    ItemEntity itemEntity = itemRepository.findById(ol.getItemId())
+                            .get();
+                    return new OrderItemEntity(itemEntity, ol.getOrderCount());
+                })
+                .collect(Collectors.toList());
 
+        // 주문 생성
         OrderEntity orderEntity = OrderEntity.builder()
-                .orderer(memberEntity)
+                .orderer(orderer)
                 .deliveryInformation(deliveryEntity)
-                .orderItemEntityList(List.of(orderItemEntity))
+                .orderItemEntityList(orderItemEntityList)
                 .build();
 
+        // 장바구니 비우기
+        List<Long> itemIdList = orderRequest.getOrderLineList().stream()
+                .map(ol -> ol.getItemId())
+                .collect(Collectors.toList());
+        cartService.removeCartLines(orderer.getMemberId(), itemIdList);
+
+        // 주문 저장
         return orderRepository.save(orderEntity).getOrderId();
     }
+
+    public void cancel(Long orderId) {
+        OrderEntity order = orderRepository.findById(orderId).get();
+        order.cancel();
+    }
+
 
 }
 
