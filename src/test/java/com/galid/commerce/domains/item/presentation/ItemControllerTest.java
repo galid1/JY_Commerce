0393e06f4f02f19b8943b0431749891d2f695180
전준엽 <galid1@naver.com>
2020-09-19 14:17:55 +0900

    mod ItemController 테스트 케이스 추가

--- src/test/java/com/galid/commerce/domains/item/presentation/ItemControllerTest.java
+++ src/test/java/com/galid/commerce/domains/item/presentation/ItemControllerTest.java
@@ -1,5 +1,8 @@
 package com.galid.commerce.domains.item.presentation;
 
+import com.galid.commerce.domains.item.domain.Book;
+import com.galid.commerce.domains.item.domain.ItemEntity;
+import com.galid.commerce.domains.item.domain.ItemQuery;
 import com.galid.commerce.domains.item.service.ItemService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +22,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(value = ItemController.class)
 @WithMockUser(value = "spring")
@@ -29,6 +31,8 @@ class ItemControllerTest {
     private MockMvc mvc;
     @MockBean
     private ItemService itemService;
+    @MockBean
+    private ItemQuery itemQuery;
 
     private Long itemId = 1l;
 
@@ -36,6 +40,21 @@ class ItemControllerTest {
     public void init() {
         given(itemService.saveItem(any()))
                 .willReturn(itemId);
+
+        ItemEntity itemEntity = createItem();
+        given(itemService.findItem(itemId))
+                .willReturn(itemEntity);
+    }
+
+    private ItemEntity createItem() {
+        return Book.builder()
+                .imagePath("test")
+                .name("test")
+                .price(1000)
+                .stockQuantity(1)
+                .isbn("test")
+                .author("test")
+                .build();
     }
 
     @Test
@@ -49,13 +68,32 @@ class ItemControllerTest {
     public void 아이템_생성() throws Exception {
         mvc.perform(post("/items/new")
                 .param("name", "TEST")
+                .param("imagePath", "TEST")
                 .param("price", "1000")
                 .param("stockQuantity", "1")
                 .param("author", "TEST")
                 .param("isbn", "TEST")
         )
-            .andExpect(view().name("redirect:/items/details?item_id=" + itemId))
-            .andExpect(status().is3xxRedirection());
+            .andExpect(status().is3xxRedirection())
+            .andExpect(view().name("redirect:/items/" + itemId));
     }
 
+    @Test
+    public void 아이템_리스트_페이지() throws Exception {
+        mvc.perform(get("/items"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("items/itemList"));
+    }
+
+    @Test
+    public void 아이템_상세_페이지() throws Exception {
+        //given
+        ItemEntity itemInModel = itemService.findItem(itemId);
+
+        //when, then
+        mvc.perform(get("/items/{itemId}", itemId))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("item", itemInModel))
+                .andExpect(view().name("items/itemDetails"));
+    }
 }
