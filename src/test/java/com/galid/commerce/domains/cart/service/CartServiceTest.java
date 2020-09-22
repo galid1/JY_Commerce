a2852d09f40ad609c0d2a01445e1a57ca8cb422b
전준엽 <galid1@naver.com>
2020-09-22 14:20:46 +0900

    mod
    - CartServiceTest 단위테스팅으로 변경

--- src/test/java/com/galid/commerce/domains/cart/service/CartServiceTest.java
+++ src/test/java/com/galid/commerce/domains/cart/service/CartServiceTest.java
@@ -1,124 +1,113 @@
 package com.galid.commerce.domains.cart.service;
 
-import com.galid.commerce.common.BaseTest;
-import com.galid.commerce.common.value.Address;
 import com.galid.commerce.domains.cart.domain.CartEntity;
+import com.galid.commerce.domains.cart.domain.CartLine;
 import com.galid.commerce.domains.cart.domain.CartRepository;
-import com.galid.commerce.domains.item.domain.Book;
-import com.galid.commerce.domains.item.domain.ItemEntity;
+import com.galid.commerce.domains.cart.domain.CheckStockQuantityService;
+import com.galid.commerce.domains.cart.query.dao.CartDao;
 import com.galid.commerce.domains.item.domain.ItemRepository;
-import com.galid.commerce.domains.member.domain.MemberEntity;
-import com.galid.commerce.domains.member.domain.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.AssertionErrors;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.util.AssertionErrors.*;
 
-class CartServiceTest extends BaseTest {
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private CartRepository cartRepository;
-    @Autowired
-    private ItemRepository itemRepository;
-
+@SpringBootTest(classes = {
+        CartService.class,
+        CheckStockQuantityService.class
+})
+class CartServiceTest {
     @Autowired
     private CartService cartService;
 
-    @Test
-    public void 장바구니_추가() throws Exception {
-        //given
-        MemberEntity member = createMember();
-        ItemEntity item = createItem();
+    @MockBean
+    private CartRepository cartRepository;
+    @MockBean
+    private CartDao cartDao;
+    @MockBean
+    private CheckStockQuantityService checkStockQuantityService;
 
-        //when
-        int orderCount = 1;
-        addToCart(member.getMemberId(), item.getItemId(), orderCount);
+    private final Long TEST_MEMBER_ID = 1l;
+    private final Long TEST_ITEM_ID = 2l;
 
-        //then
-        Map<Long, Integer> cart = cartService.getCart(member.getMemberId());
-        assertEquals(cart.get(item.getItemId()), orderCount, "장바구니에 담긴 수량과 주문수량이 일치");
+    @BeforeEach
+    public void init() {
+        CartEntity cart = new CartEntity(1l);
+        given(cartRepository.save(cart))
+                .willReturn(cart);
+        given(cartRepository.findFirstByMemberId(TEST_MEMBER_ID))
+                .willReturn(cart);
     }
 
     @Test
-    public void 장바구니_수량변경() throws Exception {
+    public void 장바구니_상품_추가() throws Exception {
         //given
-        Long memberId = createMember().getMemberId();
-        Long itemId = createItem().getItemId();
-
         int orderCount = 1;
-        addToCart(memberId, itemId, orderCount);
+        AddToCartRequestForm request = createAddToCartRequestForm(orderCount);
 
         //when
-        int newOrderCount = 2;
-        ModifyCartLineRequestForm modifyCartLineRequestForm = new ModifyCartLineRequestForm();
-        modifyCartLineRequestForm.setItemId(itemId);
-        modifyCartLineRequestForm.setOrderCount(newOrderCount);
-        cartService.modifyCartLine(memberId, modifyCartLineRequestForm);
+        cartService.addItemToCart(TEST_MEMBER_ID, request);
 
         //then
-        Map<Long, Integer> cart = cartService.getCart(memberId);
-        assertEquals(cart.get(itemId), newOrderCount, "주문수량은 변경된 주문수량과 일치");
+        Map<Long, CartLine> cart = cartRepository.findFirstByMemberId(TEST_MEMBER_ID).getCart();
+        assertEquals("장바구니에 추가된 주문 수량이 일치해야함.", cart.get(TEST_ITEM_ID).getOrderCount(), orderCount);
+    }
+
+    @Test
+    public void 장바구니_수량_변경() throws Exception {
+        //given
+        // 장바구니 담기
+        int orderCount = 1;
+        AddToCartRequestForm request = createAddToCartRequestForm(orderCount);
+        cartService.addItemToCart(TEST_MEMBER_ID, request);
+
+        int newOrderCount = 2;
+        ModifyOrderCountRequestForm modifyRequest = createModifyOrderCountRequestForm(newOrderCount);
+
+        //when
+        cartService.modifyOrderCount(TEST_MEMBER_ID, modifyRequest);
+
+        //then
+        Map<Long, CartLine> cart = cartRepository.findFirstByMemberId(TEST_MEMBER_ID).getCart();
+        assertEquals("장바구니 주문 수량이 변경한 주문수량과 일치해야함.", cart.get(TEST_ITEM_ID).getOrderCount(), newOrderCount);
+    }
+
+    private ModifyOrderCountRequestForm createModifyOrderCountRequestForm(int newOrderCount) {
+        ModifyOrderCountRequestForm modifyRequest = new ModifyOrderCountRequestForm();
+        modifyRequest.setItemId(TEST_ITEM_ID);
+        modifyRequest.setOrderCount(newOrderCount);
+
+        return modifyRequest;
+    }
+
+    private AddToCartRequestForm createAddToCartRequestForm(int orderCount) {
+        AddToCartRequestForm request = new AddToCartRequestForm();
+        request.setItemId(TEST_ITEM_ID);
+        request.setOrderCount(orderCount);
+        return request;
     }
 
     @Test
     public void 장바구니_삭제() throws Exception {
         //given
-        Long memberId = createMember().getMemberId();
-        Long itemId = createItem().getItemId();
-        addToCart(memberId, itemId, 1);
+        // 장바구니 담기
+        int orderCount = 1;
+        AddToCartRequestForm request = createAddToCartRequestForm(orderCount);
+        cartService.addItemToCart(TEST_MEMBER_ID, request);
 
         //when
-        cartService.removeItem(memberId, itemId);
+        cartService.removeCartLine(TEST_MEMBER_ID, TEST_ITEM_ID);
 
         //then
-        Map<Long, Integer> cart = cartService.getCart(memberId);
-
-        assertEquals(cart.get(itemId), null, "삭제 후 카트에 존재하지 않아야 함");
+        Map<Long, CartLine> cart = cartRepository.findFirstByMemberId(TEST_MEMBER_ID).getCart();
+        assertEquals("장바구니에서 삭제시 해당 품목은 NULL", cart.get(TEST_ITEM_ID), null);
     }
-
-    // 장바구니 추가
-    private void addToCart(Long memberId, Long itemId, int orderCount) {
-        AddToCartRequestForm addToCartRequestForm = new AddToCartRequestForm();
-        addToCartRequestForm.setItemId(itemId);
-        addToCartRequestForm.setOrderCount(orderCount);
-
-        //when
-        cartService.addToCart(memberId, addToCartRequestForm);
-    }
-
-    private MemberEntity createMember() {
-        MemberEntity memberEntity = MemberEntity.builder()
-                .authId("TEST")
-                .authPw("TEST")
-                .address(new Address("TEST", "TEST"))
-                .build();
-
-        MemberEntity saveMember = memberRepository.save(memberEntity);
-        createCart(saveMember.getMemberId());
-        return saveMember;
-    }
-
-    private void createCart(Long memberId) {
-        CartEntity cartEntity = new CartEntity(memberId);
-        cartRepository.save(cartEntity);
-    }
-
-    private ItemEntity createItem() {
-        ItemEntity itemEntity = Book.builder()
-                .author("TEST")
-                .imagePath("TEST")
-                .isbn("TEST")
-                .name("TEST")
-                .price(1000)
-                .stockQuantity(3)
-                .build();
-
-        return itemRepository.save(itemEntity);
-    }
-
-
-
 }
