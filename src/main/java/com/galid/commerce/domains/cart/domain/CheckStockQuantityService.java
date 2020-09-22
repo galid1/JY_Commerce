6a5fdbedf813a31acd3486fb6bc0c52d6d03305d
전준엽 <galid1@naver.com>
2020-09-22 13:50:57 +0900

    mod
    - 재고량 확인 도메인서비스에서 item의 남은 수량을 직접 찾아 비교하도록 변경

--- src/main/java/com/galid/commerce/domains/cart/domain/CheckStockQuantityService.java
+++ src/main/java/com/galid/commerce/domains/cart/domain/CheckStockQuantityService.java
@@ -1,18 +1,20 @@
 package com.galid.commerce.domains.cart.domain;
 
+import com.galid.commerce.domains.item.domain.ItemEntity;
+import com.galid.commerce.domains.item.domain.ItemRepository;
 import com.galid.commerce.domains.item.domain.NotEnoughStockQuantityException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CheckStockQuantityService {
-    private int orderCount;
-    private int itemStockQuantity;
+    private final ItemRepository itemRepository;
 
-    public CheckStockQuantityService(int orderCount, int itemStockQuantity) {
-        this.orderCount = orderCount;
-        this.itemStockQuantity = itemStockQuantity;
-    }
+    public void checkEnoughStockQuantity(int orderCount, Long itemId) {
+        ItemEntity itemEntity = itemRepository.findById(itemId).get();
 
-    public void checkEnoughStockQuantity() {
-        if(this.itemStockQuantity < this.orderCount)
+        if(itemEntity.getStockQuantity() < orderCount)
             throw new NotEnoughStockQuantityException();
     }
 }
