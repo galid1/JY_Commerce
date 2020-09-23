41cced29f2c7d7237c4865a8bbf6e6e288b93e2c
전준엽 <galid1@naver.com>
2020-09-23 12:04:48 +0900

    mod
    - Getter 추가
    - DiscriminatorValue 추가
    - Builder 추가, NoArgsConstructor 추가
    - 생성자 추가

--- src/main/java/com/galid/commerce/domains/item/domain/Album.java
+++ src/main/java/com/galid/commerce/domains/item/domain/Album.java
@@ -1,9 +1,23 @@
 package com.galid.commerce.domains.item.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Album")
+@Getter
+@NoArgsConstructor
 public class Album extends ItemEntity{
     private String artist;
     private String etc;
+
+    @Builder
+    public Album(String artist, String etc) {
+        this.artist = artist;
+        this.etc = etc;
+    }
 }
--- src/main/java/com/galid/commerce/domains/item/domain/Movie.java
+++ src/main/java/com/galid/commerce/domains/item/domain/Movie.java
@@ -1,9 +1,23 @@
 package com.galid.commerce.domains.item.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 @Entity
+@DiscriminatorValue("Movie")
+@Getter
+@NoArgsConstructor
 public class Movie extends ItemEntity{
     private String director;
     private String actor;
+
+    @Builder
+    public Movie(String director, String actor) {
+        this.director = director;
+        this.actor = actor;
+    }
 }
