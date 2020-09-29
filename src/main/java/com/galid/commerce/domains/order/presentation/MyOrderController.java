1ca75713437845ec856f71aa67adb58e2a599e18
전준엽 <galid1@naver.com>
2020-09-29 16:25:00 +0900

    mod
    - myOrderService에서 내 주문내역을 가져와 model에 추가하도록 변경

--- src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
+++ src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
@@ -1,9 +1,14 @@
 package com.galid.commerce.domains.order.presentation;
 
+import com.galid.commerce.domains.member.domain.MemberEntity;
 import com.galid.commerce.domains.member.service.MemberService;
+import com.galid.commerce.domains.order.service.MyOrderService;
 import com.galid.commerce.domains.order.service.OrderService;
+import com.galid.commerce.infra.AuthenticationConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +18,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MyOrderController {
     private final MemberService memberService;
     private final OrderService orderService;
+    private final MyOrderService myOrderService;
+    private final AuthenticationConverter authenticationConverter;
 
     @GetMapping("/my/orders")
-    public String getMyOrderListPage() {
+    public String getMyOrderListPage(Authentication authentication,
+                                     Model model) {
+        MemberEntity member = authenticationConverter.getMemberFromAuthentication(authentication);
+        model.addAttribute("myOrders", myOrderService.getMyOrderSummary(member.getMemberId()));
         return "orders/myOrderList";
     }
 
