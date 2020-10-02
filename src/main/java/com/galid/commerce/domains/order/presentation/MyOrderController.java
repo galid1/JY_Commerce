bf46bcb00779e6a36a49d70911a2bf9a6b667dc2
전준엽 <galid1@naver.com>
2020-10-02 20:38:45 +0900

    mod 사용하지 않는 import 제거

--- src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
+++ src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
@@ -8,7 +8,6 @@ import com.galid.commerce.domains.order.service.OrderService;
 import com.galid.commerce.infra.AuthenticationConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
