9f9a7c87efb8c0fdc5211c82dacab7b3f16651dd
전준엽 <galid1@naver.com>
2020-10-06 17:02:53 +0900

    mod
    - myOrderDetails 페이지에 모델데이터 전달

--- src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
+++ src/main/java/com/galid/commerce/domains/order/presentation/MyOrderController.java
@@ -1,10 +1,9 @@
 package com.galid.commerce.domains.order.presentation;
 
 import com.galid.commerce.domains.member.domain.MemberEntity;
-import com.galid.commerce.domains.member.service.MemberService;
+import com.galid.commerce.domains.order.query.dto.MyOrderDetailsDto;
 import com.galid.commerce.domains.order.query.dto.MyOrderSummaryDto;
 import com.galid.commerce.domains.order.service.MyOrderService;
-import com.galid.commerce.domains.order.service.OrderService;
 import com.galid.commerce.infra.AuthenticationConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -13,13 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class MyOrderController {
-    private final MemberService memberService;
-    private final OrderService orderService;
     private final MyOrderService myOrderService;
     private final AuthenticationConverter authenticationConverter;
 
@@ -33,8 +29,10 @@ public class MyOrderController {
     }
 
     @GetMapping("/my/orders/{orderId}")
-    @ResponseBody
-    public String getMyOrderDetails(@PathVariable("orderId") Long orderId) {
-        return "OK";
+    public String getMyOrderDetails(@PathVariable("orderId") Long orderId,
+                                    Model model) {
+        MyOrderDetailsDto myOrderDetails = myOrderService.getMyOrderDetails(orderId);
+        model.addAttribute("myOrderDetails", myOrderDetails);
+        return "orders/myOrderDetails";
     }
 }
