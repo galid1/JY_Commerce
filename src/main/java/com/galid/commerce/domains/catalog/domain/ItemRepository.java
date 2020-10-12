e6afc4b6f8f73ecdffc3faea1ec2689800b1598d
전준엽 <galid1@naver.com>
2020-10-12 19:04:22 +0900

    mod
    - 재고수량 부족 Exception 위치 domain에서 service 하위로 이동

--- src/main/java/com/galid/commerce/domains/catalog/domain/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.galid.commerce.domains.catalog.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
-}
