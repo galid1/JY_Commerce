cba78218573c45209f8a584616c718e55db93e49
전준엽 <galid1@naver.com>
2020-10-12 23:49:14 +0900

    add
    - Category Repository 추가

---
+++ src/main/java/com/galid/commerce/domains/catalog/domain/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.galid.commerce.domains.catalog.domain.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+}
