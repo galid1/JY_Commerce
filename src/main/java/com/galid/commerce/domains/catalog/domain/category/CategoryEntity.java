06facb4a40c0acd59223c7d2c91e9f100d3bd02f
전준엽 <galid1@naver.com>
2020-10-14 21:56:07 +0900

    mod
    - 재귀 구조에서, sub category를 별도 테이블에서 지정하도록 변경

--- src/main/java/com/galid/commerce/domains/catalog/domain/category/CategoryEntity.java
+++ src/main/java/com/galid/commerce/domains/catalog/domain/category/CategoryEntity.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "categories")
@@ -14,10 +16,16 @@ public class CategoryEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-    private Long parentId;
 
-    public CategoryEntity(String categoryName, Long parentId) {
+    @ElementCollection
+    @CollectionTable(
+            name = "sub_categories",
+            joinColumns = @JoinColumn(name = "parent_id")
+    )
+    @Column(name = "sub_category_id")
+    private List<Long> subCategories = new ArrayList<>();
+
+    public CategoryEntity(String categoryName) {
         this.categoryName = categoryName;
-        this.parentId = parentId;
     }
 }
