c4bb51bf2ccc633c49d2834a118d7d97002d8617
전준엽 <galid1@naver.com>
2020-10-16 00:12:01 +0900

    add
    - 메인 컨트롤러 추가 (이전에는 별도 모델 설정이 필요없어서, webConfigurerer를 이용해서 했었음)

---
+++ src/main/java/com/galid/commerce/domains/MainController.java
@@ -0,0 +1,34 @@
+package com.galid.commerce.domains;
+
+import com.galid.commerce.domains.catalog.domain.item.ItemQuery;
+import com.galid.commerce.domains.catalog.service.ItemSearchForm;
+import com.galid.commerce.domains.catalog.service.ItemSummaryInItemList;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+    private final ItemQuery itemQuery;
+
+    @GetMapping("/main")
+    public String getMainPage(@ModelAttribute ItemSearchForm searchForm,
+                                  Model model) {
+        // 아이템 검색 form
+        if (searchForm == null)
+            model.addAttribute("itemSearchForm", new ItemSearchForm());
+        else
+            model.addAttribute("itemSearchForm", searchForm);
+
+        // 아이템 리스트
+        List<ItemSummaryInItemList> items = itemQuery.searchItem(searchForm);
+        model.addAttribute("items", items);
+
+        return "main";
+    }
+}
