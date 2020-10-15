b5065f4ce9a27cce10e0c3db34f224c83c9d178c
전준엽 <galid1@naver.com>
2020-10-15 20:40:11 +0900

    mod
    - List 패키지 sun.tools에서 java.util로 변경

--- src/main/java/com/galid/commerce/common/config/security/DatabaseUserDetailsService.java
+++ src/main/java/com/galid/commerce/common/config/security/DatabaseUserDetailsService.java
@@ -2,9 +2,7 @@ package com.galid.commerce.common.config.security;
 
 import com.galid.commerce.domains.member.domain.MemberEntity;
 import com.galid.commerce.domains.member.domain.MemberRepository;
-import com.sun.tools.javac.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
