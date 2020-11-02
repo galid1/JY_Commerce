cf3f31b1df2e889d3cc23c7d33087c4d02fd547a
jjy <galid9619@gmail.com>
2020-11-02 17:37:30 +0900

    mod
    - 아이템 등록후, 아이템 등록됨 이벤트 발행, 각 상품의 리뷰관리 엔티티를 생성시키기 위해

--- src/main/java/com/galid/commerce/domains/catalog/service/ItemService.java
+++ src/main/java/com/galid/commerce/domains/catalog/service/ItemService.java
@@ -1,9 +1,13 @@
 package com.galid.commerce.domains.catalog.service;
 
 import com.galid.commerce.domains.catalog.domain.item.ItemEntity;
+import com.galid.commerce.domains.catalog.domain.item.ItemRegisteredEvent;
 import com.galid.commerce.domains.catalog.domain.item.ItemRepository;
+import com.galid.commerce.domains.catalog.domain.review.ReviewProductEntity;
+import com.galid.commerce.domains.catalog.domain.review.ReviewProductRepository;
 import com.galid.commerce.domains.catalog.infra.ItemFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,13 +17,19 @@ import java.util.List;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ItemService {
+    private final ApplicationContext applicationContext;
     private final ItemRepository itemRepository;
     private final ItemFactory itemFactory;
 
     @Transactional
     public Long saveItem(AddItemRequest request) {
         ItemEntity newItem = itemFactory.createItem(request);
-        return itemRepository.save(newItem).getItemId();
+        ItemEntity savedItem = itemRepository.save(newItem);
+
+        // 아이템 생성됨 이벤트 발행
+        applicationContext.publishEvent(new ItemRegisteredEvent(savedItem.getItemId()));
+
+        return savedItem.getItemId();
     }
 
     public ItemDetails findItem(Long itemId) {
