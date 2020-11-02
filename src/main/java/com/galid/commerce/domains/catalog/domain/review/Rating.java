f0bf64e78e1dd66a222916182ca2c3a1582c7581
jjy <galid9619@gmail.com>
2020-11-02 15:47:21 +0900

    mod
    - 평점 0.5 단위 제거

--- src/main/java/com/galid/commerce/domains/catalog/domain/review/Rating.java
+++ src/main/java/com/galid/commerce/domains/catalog/domain/review/Rating.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Rating {
-    HALF(0.5),
-    ONE(1.0), ONE_HALF(1.5),
-    TWO(2.0), TWO_HALF(2.5),
-    THREE(3.0), THREE_HALF(3.5),
-    FOUR(4.0), FOUR_HALF(4.5),
-    FIVE(5.0);
+    ONE(1.0), TWO(2.0), THREE(3.0), FOUR(4.0), FIVE(5.0);
 
     private Double value;
 
