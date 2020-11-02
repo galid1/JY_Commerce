b75827c476deda9a996ba6a5805928eecc3cee5e
jjy <galid9619@gmail.com>
2020-11-02 16:13:17 +0900

    mod rating Double에서 int로 변경

--- src/main/java/com/galid/commerce/domains/catalog/domain/review/Rating.java
+++ src/main/java/com/galid/commerce/domains/catalog/domain/review/Rating.java
@@ -4,11 +4,19 @@ import lombok.Getter;
 
 @Getter
 public enum Rating {
-    ONE(1.0), TWO(2.0), THREE(3.0), FOUR(4.0), FIVE(5.0);
+    ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5);
 
-    private Double value;
+    private int value;
 
-    Rating(Double value) {
+    Rating(int value) {
         this.value = value;
+    }
+
+    public static Rating valueOf(int rating) {
+        for(Rating r : Rating.values()) {
+            if (r.getValue() == rating)
+                return r;
+        }
+        return null;
     }
 }
