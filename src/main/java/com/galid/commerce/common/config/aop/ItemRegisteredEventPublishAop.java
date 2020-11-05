b0f02e0a54d14b7cb6935261233ed2e15acda585
jjy <galid9619@gmail.com>
2020-11-05 12:24:36 +0900

    add
    - 아이템 생성시 이벤트 발행 AOP 추가

---
+++ src/main/java/com/galid/commerce/common/config/aop/ItemRegisteredEventPublishAop.java
@@ -0,0 +1,23 @@
+package com.galid.commerce.common.config.aop;
+
+import com.galid.commerce.domains.catalog.domain.item.ItemRegisteredEvent;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Profile("deploy")
+@RequiredArgsConstructor
+public class ItemRegisteredEventPublishAop {
+    private final ApplicationContext context;
+
+    @AfterReturning(pointcut = "execution(* com.galid.commerce.domains.catalog.service.ItemService.saveItem(..))", returning ="returnValue")
+    public void publishItemRegisteredEvent(Object returnValue) {
+        ItemRegisteredEvent event = new ItemRegisteredEvent((Long)returnValue);
+        context.publishEvent(event);
+    }
+}
