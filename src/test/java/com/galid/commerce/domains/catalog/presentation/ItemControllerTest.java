4a3561dc65afdbc382f3f32a80f72257503cea74
jjy <galid9619@gmail.com>
2020-11-06 23:41:06 +0900

    mod
    - 가독성 향상을 위해 수정
    - model 테스트 추가

--- src/test/java/com/galid/commerce/domains/catalog/presentation/ItemControllerTest.java
+++ src/test/java/com/galid/commerce/domains/catalog/presentation/ItemControllerTest.java
@@ -2,6 +2,7 @@ package com.galid.commerce.domains.catalog.presentation;
 
 import com.galid.commerce.domains.catalog.domain.item.ItemEntity;
 import com.galid.commerce.domains.catalog.query.dao.CatalogDao;
+import com.galid.commerce.domains.catalog.service.AddItemRequest;
 import com.galid.commerce.domains.catalog.service.ItemDetails;
 import com.galid.commerce.domains.catalog.service.ItemService;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +12,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,21 +28,8 @@ class ItemControllerTest {
     private MockMvc mvc;
     @MockBean
     private ItemService itemService;
-    @MockBean
-    private CatalogDao itemQuery;
 
     private Long itemId = 1l;
-
-    @BeforeEach
-    public void init() {
-        given(itemService.saveItem(any()))
-                .willReturn(itemId);
-
-        ItemEntity itemEntity = createItem();
-
-        given(itemService.findItem(itemId))
-                .willReturn(new ItemDetails(itemEntity));
-    }
 
     private ItemEntity createItem() {
         return ItemEntity.builder()
@@ -52,30 +42,53 @@ class ItemControllerTest {
 
     @Test
     public void 아이템_생성_페이지() throws Exception {
-        mvc.perform(get("/items/new"))
+        //given
+        given(itemService.saveItem(any()))
+                .willReturn(itemId);
+
+        // when
+        ResultActions resultActions = mvc.perform(get("/items/new"));
+
+        // then
+        resultActions
+                .andExpect(model().attributeExists("form"))
                 .andExpect(view().name("items/registerItemForm"))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void 아이템_생성() throws Exception {
-        mvc.perform(post("/items/new")
+        //given
+        given(itemService.saveItem(any()))
+                .willReturn(itemId);
+
+        //when
+        ResultActions resultActions = mvc.perform(post("/items/new")
                 .param("name", "TEST")
                 .param("imagePath", "TEST")
                 .param("price", "1000")
                 .param("stockQuantity", "1")
-        )
-            .andExpect(status().is3xxRedirection())
-            .andExpect(view().name("redirect:/items/" + itemId));
+        );
+
+        //then
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/items/" + itemId));
     }
 
     @Test
     public void 아이템_상세_페이지() throws Exception {
         //given
+        given(itemService.findItem(itemId))
+                .willReturn(new ItemDetails(createItem()));
+
         ItemDetails item = itemService.findItem(itemId);
 
-        //when, then
-        mvc.perform(get("/items/{itemId}", itemId))
+        //when
+        ResultActions resultActions = mvc.perform(get("/items/{itemId}", itemId));
+
+        //then
+        resultActions
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("item", item))
                 .andExpect(view().name("items/itemDetails"));
