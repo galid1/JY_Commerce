d4df61a9eef538fc3c2e5b044cee9bdc582fc6e4
jjy <galid9619@gmail.com>
2020-11-10 12:39:19 +0900

    add
    - 주문한 상품이 아닌 리뷰작성 요청에 대한 에러 클래스

---
+++ src/main/java/com/galid/commerce/domains/catalog/domain/review/NotOrderedProductReviewException.java
@@ -0,0 +1,8 @@
+package com.galid.commerce.domains.catalog.domain.review;
+
+
+public class NotOrderedProductReviewException extends RuntimeException{
+    public NotOrderedProductReviewException(String message) {
+        super(message);
+    }
+}
