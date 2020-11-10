7a058ae171bbfaa986f7ec5a346122bf1166791c
jjy <galid9619@gmail.com>
2020-11-10 12:39:56 +0900

    mod
    - 주문한 상품에 대해서만 리뷰작성이 가능하도록 변경

--- src/main/java/com/galid/commerce/domains/catalog/service/ReviewService.java
+++ src/main/java/com/galid/commerce/domains/catalog/service/ReviewService.java
@@ -1,10 +1,16 @@
 package com.galid.commerce.domains.catalog.service;
 
 import com.galid.commerce.domains.catalog.domain.review.*;
+import com.galid.commerce.domains.catalog.query.dao.MyOrderedItemDao;
+import com.galid.commerce.domains.order.domain.OrderEntity;
+import com.galid.commerce.domains.order.domain.OrderItemEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -12,9 +18,15 @@ import javax.transaction.Transactional;
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final ReviewProductRepository reviewProductRepository;
+    private final CheckOrderedProductService checkOrderedProductService;
+    private final MyOrderedItemDao orderedProductDao;
 
     @Transactional
     public Long review(Long reviewerId, ReviewRequest reviewRequest) {
+        // 사용자가 주문한 상품목록에 리뷰대상이 존재하는지 확인
+        Set<Long> orderedItemIdSet = getOrderedItemIdSet(orderedProductDao.myOrderedListFromLastMonth(reviewerId));
+        checkOrderedProductService.checkOrderedProduct(reviewRequest.getProductId(), orderedItemIdSet);
+
         // 리뷰 생성
         ReviewEntity reviewEntity = ReviewEntity.builder()
                 .review(new Review(
@@ -34,6 +46,16 @@ public class ReviewService {
                 .rate(Rating.valueOf(reviewRequest.getRating()));
 
         return savedReview.getReviewId();
+    }
+
+    private Set<Long> getOrderedItemIdSet(List<OrderEntity> myOrderedItemIdListFromLastMonth) {
+        Set<Long> collect = myOrderedItemIdListFromLastMonth
+                .stream()
+                .map(o -> o.getOrderItemList())
+                .flatMap(olList -> olList.stream().map(ol -> ol.getItem().getItemId()))
+                .collect(Collectors.toSet());
+
+        return collect;
     }
 
 }
